a6884d9f30ea551547cead0e4dea3c2c8c88828f
mvisca-g <mvisca-g@car15s6.42barcelona.com>
2023-08-19 21:38:14 +0200

    xml implmented

---
+++ include/so_long.h
@@ -0,0 +1,9 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../minilibx/mlx.h"
+# include <unistd.h>
+
+#endif
+
+// cc main.c -L./minilibx -lmlx -I./minilibx -framework OpenGl -framework AppKit
