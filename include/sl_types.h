5a9f6da45424a7bca8f615e652c21bdd11f79905
mvisca <martin.visca@gmail.com>
2023-08-23 13:21:16 +0200

    add assets folder

--- include/sl_types.h
+++ include/sl_types.h
@@ -6,7 +6,7 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 12:39:38 by mvisca            #+#    #+#             */
-/*   Updated: 2023/08/23 13:06:55 by mvisca           ###   ########.fr       */
+/*   Updated: 2023/08/23 13:14:10 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,30 +26,30 @@ typedef enum e_cell_def
 	p_down,
 	p_left,
 	p_right
-}   t_cell;
+}   t_cell_def;
 
 typedef struct s_map
 {
-	t_cell		**e_cell;
-	int			width;
-	int			height;
+	t_cell_def		**e_cell;
+	int				width;
+	int				height;
 }	t_map;
 
 typedef struct s_img
 {
-	void		*img_ptr;
-	int			img_data;
-	int			img_size;
-	int			img_bpp;
+	void			*img_ptr;
+	int				img_data;
+	int				img_size;
+	int				img_bpp;
 }	t_img;
 
 typedef struct s_game
 {
-	void		*mlx;
-	void		*win;
-	void		*map;
-	t_img		*img;
-	int			turns;
+	void			*mlx;
+	void			*win;
+	void			*map;
+	t_img			*img;
+	int				turns;
 }	t_game;
 
 #endif
