90b99255589780454871d37b0a972c1ac4289448
mvisca <martin.visca@gmail.com>
2023-08-23 16:16:17 +0200

    test lib put image, draw pixel

--- lib_test.c
+++ lib_test.c
@@ -8,19 +8,36 @@ int	main(void)
 
 
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 500, 500, "HOLA");
-	img = (t_img *) malloc (sizeof(t_img) * 1);
-	int i = 1;
-	while (i < 100)
-	{
-		mlx_pixel_put(mlx, win, 100, i, 0xFFFF00);
-		mlx_pixel_put(mlx, win, i++, 100, 0xFF00FF);
-	}
-	img->img_ptr = mlx_xpm_file_to_image(mlx, "assets/sl_road.xpm", &img->img_w, &img->img_h);
+	win = mlx_new_window(mlx, 320, 320, "HOLA");
 
+	img = (t_img *) malloc (sizeof(t_img) * 1);
+	img->img_ptr = mlx_xpm_file_to_image(mlx, "assets/sl_road.xpm", &img->img_w, &img->img_h);
 	img->img_data = (int *)mlx_get_data_addr(img->img_ptr, &img->img_bpp, &img->img_size, &img->img_endian);
 
-	mlx_put_image_to_window(mlx, win, img->img_ptr, 10, 10);
+	int i = 0;
+	while (i < 10)
+	{
+		int j = 0;
+		while (j < 10)
+			mlx_put_image_to_window(mlx, win, img->img_ptr, 32 * i, 32 * j++);
+		i++;
+	}
+
+	i = 1;
+	int ex = 0;
+	int limit = 32 * 2;
+	while (ex < 32)
+	{
+		while (i < limit)
+		{
+			mlx_pixel_put(mlx, win, limit, i, 0xFFFF00);
+			mlx_pixel_put(mlx, win, i, limit, 0xFF00FF);
+			i++;
+		}
+		i = 1;
+		ex++;
+		limit++; 
+	}
 	mlx_loop(mlx);
 	//mlx_destroy(mlx);
 }
